af62495a2ea8ad38edd753c65c174aa6fec20222
BastianZim <10774221+BastianZim@users.noreply.github.com>
2022-03-18 22:03:58 +0100

    Migrate from old PR
    Co-Authored-By: Isuru Fernando <isuruf@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,19 +2,32 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-### Assert licenses are available
-# Install cargo-license
 export CARGO_HOME="$BUILD_PREFIX/cargo"
 mkdir $CARGO_HOME
-# Needed to bootstrap itself into the conda ecosystem
-cargo install cargo-bundle-licenses
 
-# Check that all downstream libraries licenses are present
-export PATH=$PATH:$CARGO_HOME/bin
-cargo bundle-licenses --format yaml --output CI.THIRDPARTY.yml --previous THIRDPARTY.yml --check-previous
+(
+  # Needed to bootstrap itself into the conda ecosystem
+  unset CARGO_BUILD_TARGET
+  export CARGO_BUILD_RUSTFLAGS=$(echo $CARGO_BUILD_RUSTFLAGS | sed "s@$PREFIX@$BUILD_PREFIX@g")
+  export RUSTFLAGS=$CARGO_BUILD_RUSTFLAGS
+  export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+  unset MACOSX_DEPLOYMENT_TARGET
+  unset CFLAGS
+  unset CPPFLAGS
+  unset LDFLAGS
+  unset PREFIX
+  cargo install --verbose cargo-bundle-licenses
+  # Check that all downstream libraries licenses are present
+  export PATH=$CARGO_HOME/bin:$PATH
+  cargo bundle-licenses --format yaml --output CI.THIRDPARTY.yml --previous THIRDPARTY.yml --check-previous
+)
+
+echo $CONDA_RUST_HOST, $CONDA_RUST_TARGET
+
+export RUSTFLAGS=$CARGO_BUILD_RUSTFLAGS
 
 # build statically linked binary with Rust
-cargo install --locked --root "$PREFIX" --path .
+cargo install --verbose --locked --root "$PREFIX" --path .
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/cargo-bundle-licenses"
